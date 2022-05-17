95429faf14816f6c0cada9e80c47387e0ad1c75a
nileshgulia1 <nileshgulia@gmail.com>
2022-05-17 15:23:52 +0530

    add policy config from energy theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 const applyConfig = (config) => {
+  const allowed_cors_destinations = [
+    ...(config.settings.allowed_cors_destinations || []),
+    'www.eea.europa.eu',
+    'www.eionet.europa.eu',
+    'land.copernicus.eu',
+  ];
+
+  config.settings = {
+    ...config.settings,
+    apiExpanders: [
+      ...config.settings.apiExpanders,
+      {
+        match: '/',
+        GET_CONTENT: [
+          'breadcrumbs',
+          'siblings',
+          'navigation',
+          'localnavigation',
+        ],
+      },
+    ],
+    navDepth: 4,
+    allowed_cors_destinations,
+    tableauVersion: '2.3.0',
+    pdfWorkerSrc: '//www.eea.europa.eu/pdfjs/pdf.worker.min.js',
+  };
+
   return config;
 };
 
