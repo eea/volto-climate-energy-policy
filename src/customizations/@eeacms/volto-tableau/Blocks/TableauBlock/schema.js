5f2a8892cd0d8db550a0d2047dfffa1f839d5ea1
andreiggr <andrei.ggr@gmail.com>
2023-10-05 13:54:21 +0300

    chore: eslint rules

--- src/customizations/@eeacms/volto-tableau/Blocks/TableauBlock/schema.js
+++ src/customizations/@eeacms/volto-tableau/Blocks/TableauBlock/schema.js
@@ -1,5 +1,5 @@
-// eslint-disable import/no-anonymous-default-export
-// eslint-disable-next-line import/no-unresolved
+/* eslint-disable import/no-anonymous-default-export, import/no-unresolved */
+
 import {
   getSheetnamesChoices,
   canChangeVizData,
